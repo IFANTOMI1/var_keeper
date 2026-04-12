1bdcfa98c450c68f6b29406a72f8e696c9a9da7b
Osman Nuri <muspaevo3@gmail.com>
2026-04-12 13:45:26 +0300

    Добавлен pull-request-to-main.yml

--- test/integration-tests.sh
+++ test/integration-tests.sh
@@ -1,6 +1,15 @@
-ANSW=$(curl http://127.0.0.1:5000/)
-if [ "$ANSW" != "Hello, World!" ]; then
+#!/bin/bash
+
+echo "Waiting 3 seconds for Flask..."
+sleep 3
+
+echo "Making curl request to http://127.0.0.1:5000"
+ANSW=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:5000)
+
+if [ "$ANSW" != "200" ]; then
+  echo "Integration Test FAIL. HTTP code: $ANSW"
   exit 1
 else
   echo "Integration Test PASS"
+  exit 0
 fi
